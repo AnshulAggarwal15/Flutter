7f0de2884bad530db0b2bfa823f629f384814198
Anshul <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-02-19 23:52:24 +0530

    Create SnackBar.dart

---
+++ Playing with UI/Buttons/SnackBar.dart
@@ -0,0 +1,85 @@
+#include 'package:flutter/material.dart
+
+//SnackBar is a small message generally shown at the bottem of an application after user perfroms an action in the app
+
+void showSnackBar(BuildContext context, String item) {
+	var snackBar = SnackBar(
+		content: Text("You just tapped $item"),
+		action: SnackBarAction(
+				label: "UNDO",
+				onPressed: () {
+					debugPrint('Performing dummy UNDO operation');
+				}
+		),
+	);
+
+	Scaffold.of(context).showSnackBar(snackBar);
+}
+
+//Eaxample
+
+/*
+import 'package:flutter/material.dart';
+import './app_screens/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+
+	  title: "Exploring UI widgets",
+
+	  home: Scaffold(
+      appBar: AppBar(title: Text("Long List"),),
+      body: getListView(),
+      floatingActionButton: FloatingActionButton(
+	      onPressed: () {
+	      	debugPrint("FAB clicked");
+	      },
+	      child: Icon(Icons.add),
+	      tooltip: 'Add One More Item',
+      ),
+    ),
+
+  ));
+}
+
+void showSnackBar(BuildContext context, String item) {
+	var snackBar = SnackBar(
+		content: Text("You just tapped $item"),
+		action: SnackBarAction(
+				label: "UNDO",
+				onPressed: () {
+					debugPrint('Performing dummy UNDO operation');
+				}
+		),
+	);
+
+	Scaffold.of(context).showSnackBar(snackBar);
+}
+
+List<String> getListElements() {
+
+	var items = List<String>.generate(1000, (counter) => "Item $counter");
+	return items;
+}
+
+Widget getListView() {
+
+	var listItems = getListElements();
+
+	var listView = ListView.builder(
+			itemBuilder: (context, index) {
+
+				return ListTile(
+					leading: Icon(Icons.arrow_right),
+					title: Text(listItems[index]),
+					onTap: () {
+						showSnackBar(context, listItems[index]);
+					},
+				);
+			}
+	);
+
+	return listView;
+}
+
+*/
