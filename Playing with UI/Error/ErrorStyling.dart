658d6890376eee69b638f59309ada3dde4295760
Anshul <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-02-23 23:32:41 +0530

    Create ErrorStyling.dart

---
+++ Playing with UI/Error/ErrorStyling.dart
@@ -0,0 +1,28 @@
+//Here we will learn how to customize the error you want to show
+
+//example
+
+Padding(
+                    padding: EdgeInsets.only(
+                        top: _minimumPadding, bottom: _minimumPadding),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      style: textStyle,
+                      controller: principalController,
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter principal amount';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Principal',
+                          hintText: 'Enter Principal e.g. 12000',
+                          labelStyle: textStyle,
+                          //error styling
+                          errorStyle: TextStyle(
+                            color: Colors.yellowAccent,
+                            fontSize: 15.0
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    )),
